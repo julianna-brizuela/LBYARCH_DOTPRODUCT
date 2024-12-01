c07ade7f1c85ad2cf769cd6073866715380de179
julianna-brizuela <julianna_brizuela@dlsu.edu.ph>
2024-12-01 21:24:27 +0800

    Update to program flow

--- ALVAREZBRIZUELA-LBYARCHMCO2.c
+++ ALVAREZBRIZUELA-LBYARCHMCO2.c
@@ -17,19 +17,18 @@ float dot_product(float * a, float * b, int n) {
 
 void initialize_arrays(float * a, float * b, int n) {
 	int i;
-	
-	srand(time(NULL)); 
-	
+
 	for (i = 0; i < n; i++) {
-		a[i] = ((float)rand() / (float)RAND_MAX) * 100.0f;
-        b[i] = ((float)rand() / (float)RAND_MAX) * 100.0f;
+		a[i] = (float)rand() / RAND_MAX;
+        b[i] = (float)rand() / RAND_MAX;
 	}
 }
 
 int main() {
 	// time declarations
-	clock_t startTime, endTime;
-	double totalTime;
+	srand(time(NULL)); 
+	clock_t startTimeC, endTimeC;
+	clock_t startTimeASM, endTimeASM;
 	
 	// user input
 	int input;
@@ -63,10 +62,9 @@ int main() {
 			break;
 	}
 
-	a=(float*)malloc(vector_size*sizeof(float));
-	b=(float*)malloc(vector_size*sizeof(float));
-	initialize_arrays(a, b, vector_size);
-
+	// MEM ALLOCATION
+	a = (float*)malloc(vector_size*sizeof(float));
+	b = (float*)malloc(vector_size*sizeof(float));
 	
 	if(a && b) {
 		printf("Memory successfully allocated!\n");
@@ -77,47 +75,74 @@ int main() {
 	
 	printf("\n--------------------\n");
 	
-	int count = 0;
+	input = 0;
 	
-	while (input != 3) {
-		// kernel initialization
-		sdot = 0.0;
-		count= count + 1;
-		printf("\nEXECUTION %d: Which kernel would you like to run?\n[1] C Kernel\n[2] ASM Kernel\n[3] Exit\nInput: ", count);
+	while (input != 2) {
+		printf("\nExecute? [1] Yes [2] No\nInput: ");
 		scanf("%d", &input);
 		
-		switch(input) {
+		printf("\n--------------------\n");
+		
+		switch (input) {
 			case 1:
-				startTime = clock();
-				sdot = dot_product(a, b, vector_size);
-				printf("sdot is: %f\n", sdot);
-				endTime = clock();
+				// EXECUTION
+				sdot = 0.0;
+				int count = 0;
+				double totalTimeC = 0.0;
+				double avgTimeC = 0.0;
+				double totalTimeASM = 0.0;
+				double avgTimeASM = 0.0;
 				
-				totalTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
-				printf("Total time taken to compute for the dot product is: %f\n", totalTime);
+				initialize_arrays(a, b, vector_size);
+				printf("C KERNEL sdot is: ");
+			
+				// 20 ITERATIONS OF C KERNEL
+				for (count = 0; count < 20; count++) {
+					startTimeC = clock();
+					sdot = dot_product(a, b, vector_size);
+					endTimeC = clock();
+					totalTimeC += (double)(endTimeC - startTimeC) / CLOCKS_PER_SEC;
+				}
+				
+				printf("%.2f\n", sdot);
+				
+				avgTimeC = totalTimeC / 20;
+				
+				printf("C KERNEL: Average time taken to compute for the dot product for 20 iterations is: %f\n", avgTimeC);
+				printf("C KERNEL: Total time taken to compute for the dot product is: %f\n", totalTimeC);
+				printf("\n--------------------\n");
+				
+				printf("ASM KERNEL sdot is: ");
+				
+				// 20 ITERATIONS OF ASM KERNEL
+				for (count = 0; count < 20; count++) {
+					startTimeASM = clock();
+					sdot = dotProd(a, b, vector_size);
+					endTimeASM = clock();
+					
+					totalTimeASM += (double)(endTimeASM - startTimeASM) / CLOCKS_PER_SEC;
+				}
+				
+				printf("%.2f\n", sdot);
+				
+				avgTimeASM = totalTimeASM / 20;
+																	                                         
+				printf("ASM KERNEL: Average time taken to compute for the dot product for 20 iterations is: %f\n", avgTimeASM);
+				printf("ASM KERNEL: Total time taken to compute for the dot product is: %f\n", totalTimeASM);
 				printf("\n--------------------\n");
 				break;
 			case 2:
-				startTime = clock();
-				sdot = dotProd(a, b, vector_size);
-				printf("sdot is: %f\n", sdot);
-				endTime = clock();
-				
-				totalTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
-				printf("Total time taken to compute for the dot product is: %f\n", totalTime);
-				printf("\n--------------------\n");
-				break;
-			case 3:
-				printf("Bye bye, my goat Sir Hiroki.");
 				break;
 			default:
 				break;
-		}	
+		}
 	}
 	
 	
 	free(a);
 	free(b);
 	
+	
 	return 0;
-}
+	}
+
